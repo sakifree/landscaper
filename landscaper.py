da467e36cce7fb252b3b7ce4d00e0d0e6ddd2409
Sakinah Freeman <freemansakinah@gmail.com>
2023-02-05 18:11:03 -0500

    first commit

---
+++ landscaper.py
@@ -0,0 +1,11 @@
+while(True):
+    user_choice = input("[1] Mow Lawn [2] Check Stats [Q] Quit ")
+    
+    if(user_choice == 1):
+        pass
+    elif(user_choice == 2):
+        pass
+    elif(user_choice == "Q"):
+        break
+    else:
+        print("Invalid input. Please select 1, 2, or Q.")
