80c4c074debfedb7c093215ee2cc507dd79a93e9
Sakinah Freeman <freemansakinah@gmail.com>
2023-02-05 20:06:23 -0500

    determines initial game state

--- landscaper.py
+++ landscaper.py
@@ -1,3 +1,15 @@
+## Game State
+
+game = {"tool": 0, "money": 0}
+
+tools = [
+    {"name": "teeth", "profit": 1, "cost": 0},
+    {"name": "rusty scissors", "profit": 5, "cost": 5},
+    {"name": "old-timey push mower", "profit": 50, "cost": 25},
+    {"name": "fancy battery-powered mower", "profit": 100, "cost": 250},
+    {"name": "hire team of starving students", "profit": 250, "cost": 500}
+]
+
 while(True):
     user_choice = input("[1] Mow Lawn [2] Check Stats [Q] Quit ")
     
